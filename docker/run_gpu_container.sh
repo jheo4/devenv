0c55273f099ffb4936fcad22276dde3e3fc81090
Jin Heo <993jin@gmail.com>
2024-10-21 16:47:18 -0400

    update

--- docker/run_gpu_container.sh
+++ docker/run_gpu_container.sh
@@ -6,12 +6,27 @@ if [ "$#" -ne 2 ]; then
   exit 1
 fi
 
-docker run -it \
-  --net=host \
-  --ipc=host \
-  --gpus all \
-  -e NVIDIA_DRIVER_CAPABILITIES=all \
-  -e DISPLAY=$DISPLAY \
-  -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
-  --privileged \
-  --name $1 $2
+if [ "$#" -eq 2]; then
+  docker run -it \
+    --net=host \
+    --ipc=host \
+    --gpus all \
+    -e NVIDIA_DRIVER_CAPABILITIES=all \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
+    --privileged \
+    --name $1 $2
+elif [ "$#" -eq 3]; then
+  docker run -it \
+    --net=host \
+    --ipc=host \
+    --gpus all \
+    -e NVIDIA_DRIVER_CAPABILITIES=all \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
+    -v ~/mnt/:/home/jin/mnt \
+    -v ~/mnt/Data:/home/jin/mnt/Data \
+    -v ~/.ssh/id_ed25519:/home/jin/.ssh/id_ed25519 \
+    --privileged \
+    --name $1 $2
+fi
