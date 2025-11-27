757b77e4981c7176ee15145ae763e28cc318c68f
Jin Heo <993jin@gmail.com>
2025-11-26 23:50:16 -0500

    update ubuntu host

---
+++ ubuntu/24_04.sh
@@ -0,0 +1,53 @@
+# nodejs
+sudo apt install -yf software-properties-common sudo netcat-openbsd curl git perl
+sudo add-apt-repository ppa:neovim-ppa/unstable && sudo apt update
+sudo snap install node --classic
+
+# yarn
+sudo npm install --global yarn
+sudo npm i -g bash-language-server
+
+# dev tools
+sudo apt install -yf \
+  byobu git gdb gcc g++ python3 python3-pip llvm clang htop snapd \
+  neovim \
+  wget zsh fonts-powerline \
+  build-essential cmake python3-dev virtualenv default-jdk exuberant-ctags silversearcher-ag \
+  w3m caca-utils highlight atool poppler-utils mediainfo iputils-ping
+sudo apt install -yf clangd
+
+# monitoring tools
+sudo snapd install bpytop
+sudo apt install -yf linux-tools-common linux-tools-generic linux-tools-$(uname -r nload)
+sudo apt install pipx
+sudo pipx install diff-highlight
+
+# clagd
+sudo ln -s /usr/lib/llvm-18/lib/libclang.so.1 /usr/lib/llvm-18/lib/libclang.so
+
+# shell & vim setup
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+git clone https://github.com/powerline/fonts && cd fonts && ./install.sh && cd ~
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+chsh -s $(which zsh)
+
+git clone https://github.com/jheo4/devenv.git ~/devenv
+
+# mig to lazyvim
+# cp ~/devenv/app_conf/init.vim ~/.config/nvim
+# cp ~/devenv/app_conf/coc-settings.json ~/.config/nvim
+# cp -rf ~/devenv/app_conf/vim-include ~/.config/nvim
+mkdir --parents ~/.config/nvim
+git clone git@github.com:jheo4/nvim_config.git ~/.config/nvim && . ~/.config/nvim/install_font.sh
+
+cp ~/devenv/app_conf/gitconfig ~/.gitconfig
+cp ~/devenv/app_conf/zshrc ~/.zshrc
+cp ~/devenv/app_conf/XTerm ~/XTerm
+cp ~/devenv/app_conf/gitmessage.txt ~/.gitmessage.txt
+git config --global commit.template ~/.gitmessage.txt
+
+cd ~/.fzf && ./install
